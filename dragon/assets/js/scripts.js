90e2c43e6eb3e144dbd533fbe207a3bfb812164d
wgrevis <wgrevis@ualr.edu>
2022-12-13 02:18:02 +0000

    some changes

---
+++ dragon/assets/js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    $('.gallery').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      slide: 'div',
+      cssEase: 'linear',
+      autoplay: true,
+      autoplaySpeed: 2000
+    });
+  
+  
+  });
+
